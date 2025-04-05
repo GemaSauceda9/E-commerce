ac6574b455f93c08bf56d5aea71363a6a57595cf
Gema Sauceda <miaestephania97@gmail.com>
2025-04-05 13:54:33 -0600

    Se crearon los recursos para los modelos correspondientes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\EtiquetaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +19,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// * Rutas para productos
+Route::resource('productos', ProductoController::class);
+
+// * Rutas para categorías
+Route::resource('categorias', CategoriaController::class);
+
+// * Rutas para etiquetas
+Route::resource('etiquetas', EtiquetaController::class);
 
 require __DIR__.'/auth.php';
