a756710b9e1ab288d9228a9e239cc61eab9e38ca
Gema <rushersauceda@gmsil.com>
2025-05-25 16:14:03 -0600

    Primer commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,13 +21,46 @@ class UserSeeder extends Seeder
             'tipo_usuario' => 'admin',
         ]);
 
+         // Crear usuario 2 administrador
+         User::factory()->create([
+            'name' => 'Reyner',
+            'email' => 'reyner@email.com',
+            'password' => Hash::make('password'),
+            'tipo_usuario' => 'admin',
+        ]);
+
         // Crear usuario cliente
         User::factory()->create([
-            'name' => 'Cliente',
+            'name' => 'Cliente 1',
             'email' => 'cliente@example.com',
             'password' => Hash::make('password'),
             'tipo_usuario' => 'cliente',
         ]);
+         
+        // Crear usuario cliente 2
+        User::factory()->create([
+            'name' => 'Cliente 2',
+            'email' => 'cliente@example.com',
+            'password' => Hash::make('password'),
+            'tipo_usuario' => 'cliente',
+        ]);
+
+        // Crear usuario cliente 3
+        User::factory()->create([
+            'name' => 'Cliente 3',
+            'email' => 'cliente@example.com',
+            'password' => Hash::make('password'),
+            'tipo_usuario' => 'cliente',
+        ]);
+
+         // Crear usuario cliente 4
+         User::factory()->create([
+            'name' => 'Cliente 4',
+            'email' => 'cliente@example.com',
+            'password' => Hash::make('password'),
+            'tipo_usuario' => 'cliente',
+        ]);
+
 
         // Crear varios usuarios cliente aleatorios
         User::factory(5)->create([
